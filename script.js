c35cc7a8f34b4c52b1e2fc097ce7731c5e84d076
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-01-31 15:39:53 -0300

    Resolvido desafio 4 de busca por artistas e corrigido erros que estavam atrapalhando a busca

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Pega os elementos no html com os ids especificados e armazena nas constantes
 const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById('result-artist');
-const resultPlaylist = document.getElementById('result-playlists');
+const resultPlaylists = document.getElementById('result-playlists');
 
 // a função recebe o searchTerm, que é o termo digitado no input de pesquisa e procura na api
 function requestApi(searchTerm) {
@@ -11,31 +11,71 @@ function requestApi(searchTerm) {
   // consome a api utilizando promisses
   fetch(url)
     .then((response) => response.json())
-    .then((result) => displayResults(result));
+    .then((result) => displayResults(result, searchTerm));
 }
 
-function displayResults(result) {
-  resultPlaylist.classList.add('hidden'); // adiciona a classe hidden ao elemento com a classe result-playlists
-  const artistName = document.getElementById('artist-name'); // pega o nome do artista
-  const artistImage = document.getElementById('artist-img');
+function displayResults(result, searchTerm) {
+  resultPlaylists.classList.add('hidden'); // adiciona a classe hidden ao elemento com a classe result-playlists
+  //   const artistName = document.getElementById('artist-name'); // pega o nome do artista
+  //   const artistImage = document.getElementById('artist-img');
 
-  result.forEach((element) => {
-    artistName.innerText = element.name; // pega o texto do nome na api
-    artistImage.src = element.urlImg; // pega a imagem do artista na api
+  //   result.forEach((element) => {
+  //     artistName.innerText = element.name; // pega o texto do nome na api
+  //     artistImage.src = element.urlImg; // pega a imagem do artista na api
+  //   });
+
+  // Seleciona o elemento no html que será colocado os dados da api
+  const gridContainer = document.querySelector('.grid-container');
+  gridContainer.innerHTML = ''; // Limpa os resultados anteriores
+
+  // Cria o filtro com base no que é pesquisado
+  const filteredArtists = result.filter((artist) =>
+    artist.name.toLowerCase().includes(searchTerm)
+  );
+
+  // Se não houver artistas digitados na pesquisa, é ocultada a área de resultados
+  if (filteredArtists.length === 0) {
+    gridContainer.innerHTML = '<p>No artists found</p>';
+  }
+
+  filteredArtists.forEach((artist) => {
+    const artistCard = document.createElement('div');
+    artistCard.classList.add('artist-card');
+
+    artistCard.innerHTML = `
+    <div class="card-img">
+    <img class="artist-img" src="${artist.urlImg}" />
+    <div class="play">
+    <span class="fa fa-solid fa-play"></span>
+    </div>
+    </div>
+    <div class="card-text">
+    <span class="artist-name">${artist.name}</span>
+    <span class="artist-categorie">Artista</span>
+    </div>
+    `;
+
+    gridContainer.appendChild(artistCard); // Cria o elemento artistCard no html
   });
-
   resultArtist.classList.remove('hidden'); // remove a classe hidden do elemento com a classe result-artist
 }
 
 // aguarda até que seja digitado no input de pesquisa, e quando isso acontece, executa a arrow function
 document.addEventListener('input', () => {
   // Declaração da constante que recebe os valores digitados pelo usuário no input
-  const searchTerm = searchInput.value.toLowerCase();
+  const searchTerm = searchInput.value.toLowerCase().trim(); // trim remove espaços vazios
 
   // se o termo de pesquisa estiver vazio, faça...
   if (searchTerm === '') {
-    resultPlaylist.classList.add('hidden'); // adiciona a classe hidden ao elemento com a classe result-playlists
-    resultArtist.classList.remove('hidden'); // remove a classe hidden do elemento com a classe result-artist
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.innerHTML = '';
+    // resultArtist.classList.remove('hidden'); // remove a classe hidden do elemento com a classe result-artist
+    // resultPlaylists.classList.add('hidden'); // adiciona a classe hidden ao elemento com a classe result-playlists
+
+    resultArtist.classList.add('hidden'); // adiciona a classe hidden ao elemento com a classe result-artist
+
+    resultPlaylists.classList.remove('hidden'); // remove a classe hidden do elemento com a classe result-playlists
+
     return; // finaliza a execução após ter feito tudo
   }
 
